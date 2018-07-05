d6c4a57e586c46e18d6c4dbd166e02a1b1c57ebb
kylehlw <kylehlw@gmail.com>
2018-07-05 11:19:50 +0800

    add new user agent

--- DouBanSpider2.py
+++ DouBanSpider2.py
@@ -18,6 +18,7 @@ class BooksSpider:
             'User-Agent': "Mozilla/5.0 (Windows NT 6.2) AppleWebKit/535.11 (KHTML, like Gecko) Chrome/17.0.963.12 "
                           "Safari/535.11"},
         {'User-Agent': 'Mozilla/5.0 (compatible; MSIE 10.0; Windows NT 6.2; Trident/6.0)'},
+        {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/67.0.3396.99 Safari/537.36'},
         {'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:40.0) Gecko/20100101 Firefox/40.0'},
         {
             'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Ubuntu '
